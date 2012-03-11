83767b814d1896d908fdd7b6e8ca1951baa7d57f
Roger Wang <wangxiong@msn.com>
2012-03-11 19:08:30 +0800

    Windows 8 Hands on Lab Parctives on Microsoft China

---
+++ Source/js/data.js
@@ -0,0 +1,41 @@
+﻿(function () {
+    "use strict";
+
+    function groupKeySelector(item) {
+        return item.group.key;
+    }
+
+    function groupDataSelector(item) {
+        return item.group;
+    }
+
+    // This function returns a WinJS.Binding.List containing only the items
+    // that belong to the provided group.
+    function getItemsFromGroup(group) {
+        return list.createFiltered(function (item) { return item.group.key === group.key; });
+    }
+
+    var list = new WinJS.Binding.List();
+    var groupedItems = list.createGrouped(groupKeySelector, groupDataSelector);
+
+    // TODO: Replace the data with your real data.
+    // You can add data from asynchronous sources whenever it becomes available.
+    WinJS.xhr({ url: "../data/Recipes.txt" })
+	.then(function (xhr) {
+	    var items = JSON.parse(xhr.responseText);
+
+	    // Add the items to the WinJS.Binding.List
+	    items.forEach(function (item) {
+	        list.push(item);
+        });
+
+        ContosoCookbook.postActivate();
+
+	});
+
+    WinJS.Namespace.define("data", {
+        items: groupedItems,
+        groups: groupedItems.groups,
+        getItemsFromGroup: getItemsFromGroup
+    });
+})();
---
+++ Source/js/media.js
@@ -0,0 +1,28 @@
+﻿(function () {
+    "use strict";
+
+    var dtm = Windows.ApplicationModel.DataTransfer.DataTransferManager;
+    var capture = Windows.Media.Capture;
+
+    WinJS.Namespace.define("media", {
+        shareFile: null, // Photo or video to be shared
+
+        sharePhoto: function () {
+            dtm.showShareUI();
+        },
+
+        shareVideo: function () {
+            var camera = new capture.CameraCaptureUI();
+            camera.videoSettings.format = capture.CameraCaptureUIVideoFormat.wmv;
+
+            // Capture a video and display the share UI
+            camera.captureFileAsync(capture.CameraCaptureUIMode.video)
+	    .then(function (file) {
+	        if (file != null) {
+	            media.shareFile = file;
+	            dtm.showShareUI();
+	        }
+	    });
+        }
+    });
+})();
